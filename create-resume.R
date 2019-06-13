c73093be607f4e61c7672d7aeaa8b8ed586d40c8
marinuslouw <marinus@exegetic.biz>
2019-06-13 06:36:46 +0200

    Add experience and fix template to be identical to base template.

--- create-resume.R
+++ create-resume.R
@@ -12,6 +12,28 @@ data <- fromJSON(file.path("json", JSONFILE))
 
 data$tools <- lapply(data$tools, function(tool) paste0('<div class="col-md-4">', tool, '</div>')) %>% paste(collapse = "\n")
 
+# EXPERIENCE ----------------------------------------------------------------------------------------------------------
+
+# Replace table with formatted HTML.
+#
+data$experience <- lapply(1:nrow(data$experience), function(n) {
+  attach(data$experience[n, ])
+  # 
+  html <- glue('
+  <div>
+    <h3 style="padding-top:10px;">{company}</h3>
+    <div class="row">
+      <div class="emph col">{title}</div>
+      <div class="col-right light">{period}</div>
+    </div>
+    <div>{details}</div>
+  </div>')
+  # 
+  detach()
+  # 
+  html
+}) %>% paste(collapse = "\n")
+
 # EDUCATION -----------------------------------------------------------------------------------------------------------
 
 # Replace table with formatted HTML.
@@ -21,13 +43,34 @@ data$education <- lapply(1:nrow(data$education), function(n) {
   # 
   html <- glue('
   <div>
-    <h3>{qualification}</h3>
+    <h3 style="padding-top:10px;">{qualification}</h3>
     <div>{institution}</div>
     <div class="row">
       <div class="col light">{location}</div>
       <div class="col-right light">{period}</div>
     </div>
     <div>{details}</div>
+  </div>')
+  # 
+  detach()
+  # 
+  html
+}) %>% paste(collapse = "\n")
+  
+# ABOUT ---------------------------------------------------------------------------------------------------------------
+
+# Replace table with formatted HTML.
+#
+data$about <- lapply(1:nrow(data$about), function(n) {
+  attach(data$about[n, ])
+  # 
+  html <- glue('
+  <div>
+    <h3>Spoken Languages</h3>
+    <div>{languages}</div>
+    </div>
+    <h3 style="padding-top:10px;">Hobbies</h3>
+    <div>{hobbies}</div>
   </div>')
   # 
   detach()
